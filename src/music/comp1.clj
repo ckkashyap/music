d213082fe72bc42c585f31c8e74da6a362d87b35
kashyap <ckkashyap@gmail.com>
2014-01-26 10:43:20 +0530

    chord added

--- src/music/comp1.clj
+++ src/music/comp1.clj
@@ -12,12 +12,27 @@
 
 
 
-(defmethod play-note :thePart [{midi :pitch}] (sampled-piano midi))
+(defmethod play-note :thePart [{midi :pitch}] (sampled-piano midi :amp 0.5))
+(defmethod play-note :theStrumming [{midi :pitch}] (snth/overpad midi :amp 0.2))
+
+(def strumming
+  (->>
+    (phrase [   1/3 1/2 1/3 1/2 1/2 3/4 3/4 1/2 3/4 1/2]
+            [   [9 12 16]  nil   nil   nil   nil   [9 12 16]   nil   nil   [9 12 16]   [ 7 11 14] ])
+
+    (then (phrase
+            [1    1/3 1/2 1/3 1/2 1/2 3/4 3/4 1/2 3/4]
+            [nil   [9 12 16]  nil   nil   nil   nil   [9 12 16]   nil   nil   [ 8 11 14]  ]))
+
+
+    (where :part (is :theStrumming))))
+
 
 (def melody
   (->>
-    (phrase [1/3 1/2 1/3 1/2 1/2 3/4 3/4 1/2 3/4 1/2]
-            [9  9   12   11   9   9   11   9   7   2])
+
+    (phrase [   1/3 1/2 1/3 1/2 1/2 3/4 3/4 1/2 3/4 1/2]
+            [   9   9   12   11   9   9   11   9   7   2])
 
     (then (phrase
             [1    1/3 1/2 1/3 1/2 1/2 3/4 3/4 1/2 3/4]
@@ -27,6 +42,7 @@
 
 (->>
   melody
+  (with strumming)
     (canon (comp (simple 0)
                  (partial where :part (is :thePart))))
 
